8c35d89f9d8e7e364dcd28f9f7c85b1efffad835
BartWiniarski <bartosz.winiarski@poczta.fm>
2023-12-20 16:29:06 +0100

    fakeUsersGenerator and PrepareDB completed

--- src/main/java/pl/mydojo/Application.java
+++ src/main/java/pl/mydojo/Application.java
@@ -2,20 +2,37 @@ package pl.mydojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import pl.mydojo.app.utils.FakeGenerators;
+import pl.mydojo.app.utils.PrepareDb;
 
 @SpringBootApplication
 public class Application {
 
-
 	public static void main(String[] args) {
-		usersGenerator();
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		prepareDb(context);
 	}
 
-	public static void springBoot(String[] args){
-		SpringApplication.run(Application.class, args);
+	private static void prepareDb(ApplicationContext context) {
+		PrepareDb prepareDb = context.getBean(PrepareDb.class);
+		prepareDb.setRoles();
+		prepareDb.setBasicAdmin();
+		prepareDb.setBasicTrainer();
+		prepareDb.setBasicStudent();
+		generateFakeUsers(context);
 	}
 
-	public static void usersGenerator(){
-		System.out.println("test");
+	public static void generateFakeUsers(ApplicationContext context){
+		FakeGenerators fakeGenerators = context.getBean(FakeGenerators.class);
+		fakeGenerators.usersGenerator("admin",1,30,35);
+		fakeGenerators.usersGenerator("student",5,5,10);
+		fakeGenerators.usersGenerator("student",5,10,15);
+		fakeGenerators.usersGenerator("student",5,15,20);
+		fakeGenerators.usersGenerator("student",5,20,60);
+		fakeGenerators.usersGenerator("trener",5,22,80);
+
 	}
+
+
 }

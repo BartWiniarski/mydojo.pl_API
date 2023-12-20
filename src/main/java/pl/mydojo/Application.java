f9fbab90c66df88caae24e39f201c2a2c45f15ed
BartWiniarski <bartosz.winiarski@poczta.fm>
2023-12-20 14:18:34 +0100

    initial commit

--- src/main/java/pl/mydojo/Application.java
+++ src/main/java/pl/mydojo/Application.java
@@ -6,8 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+
 	public static void main(String[] args) {
+		usersGenerator();
+	}
+
+	public static void springBoot(String[] args){
 		SpringApplication.run(Application.class, args);
 	}
 
+	public static void usersGenerator(){
+		System.out.println("test");
+	}
 }
